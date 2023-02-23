62dff53259b42eceff45773d8fdd6ddd1fdab2a6
Leying Hu <leying.hu@innopeaktech.com>
2023-02-22 18:40:34 -0800

    add load motion func to support AMASS

--- dataset/load_test_anim.py
+++ dataset/load_test_anim.py
@@ -6,8 +6,24 @@ def load_test_anim(filename, device):
     anim = np.load(filename)
     anim = torch.tensor(anim, device=device, dtype=torch.float)
     poses = anim[:, :-3]
+    assert poses.shape[1] == 72
     loc = anim[:, -3:]
     loc[..., 1] += 1.1174
     loc = loc.unsqueeze(1)
 
     return poses, loc
+
+
+def load_cmu_mocap_anim(filename, device):
+    # 72 of 156 poses are picked from
+    # https://github.com/NetEase-GameAI/MoCap-Solver/blob/main/SyntheticDataGeneration/generate_test_data.py#L139
+    cdata = torch.from_numpy(np.load(filename)["poses"][:, :])
+    N, D = cdata.shape
+    assert D == 156
+
+    poses = torch.zeros((N, 72), device=device, dtype=torch.float)
+    poses[:, :66] = cdata[:, :66]
+    poses[:, 66:69] = cdata[:, 75:78]
+    poses[:, 69:72] = cdata[:, 120:123]
+
+    return poses
--- demo.py
+++ demo.py
@@ -10,7 +10,7 @@ from models.kinematics import ForwardKinematics
 from models.transforms import aa2mat
 from models.deformation import deform_with_offset
 from dataset.mesh_dataset import StaticMeshes, parent_smpl
-from dataset.load_test_anim import load_test_anim
+from dataset.load_test_anim import load_test_anim, load_cmu_mocap_anim
 from dataset.bvh_writer import WriterWrapper
 from dataset.obj_io import write_obj
 from option import TrainingOptionParser
@@ -19,15 +19,19 @@ from tqdm import tqdm
 
 def get_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--device', type=str, default='cpu')
-    parser.add_argument('--pose_file', type=str, default='./eval_constant/sequences/house-dance.npy')
-    parser.add_argument('--model_path', type=str, default='./pre_trained')
-    parser.add_argument('--obj_path', type=str, default='./eval_constant/meshes/artist-2.obj')
-    parser.add_argument('--result_path', type=str, default='./demo')
-    parser.add_argument('--normalize', type=int, default=0)
-    parser.add_argument('--envelope_only', type=int, default=0)
-    parser.add_argument('--animated_bvh', type=int, default=0)
-    parser.add_argument('--obj_output', type=int, default=1)
+    parser.add_argument("--device", type=str, default="cpu")
+    parser.add_argument(
+        "--pose_file", type=str, default="./eval_constant/sequences/house-dance.npy"
+    )
+    parser.add_argument("--model_path", type=str, default="./pre_trained")
+    parser.add_argument(
+        "--obj_path", type=str, default="./eval_constant/meshes/artist-2.obj"
+    )
+    parser.add_argument("--result_path", type=str, default="./demo")
+    parser.add_argument("--normalize", type=int, default=0)
+    parser.add_argument("--envelope_only", type=int, default=0)
+    parser.add_argument("--animated_bvh", type=int, default=0)
+    parser.add_argument("--obj_output", type=int, default=1)
     return parser
 
 
@@ -44,22 +48,34 @@ def eval_residual(t_pose, topo_id, pose, model: BlendShapesGenerate):
     with torch.no_grad():
         t_pose = t_pose.unsqueeze(0)
         model.forward(t_pose, pose, topo_id, None)
-    return model.offsets[0], model.models['bs'].basis_full, model.models['bs'].coff
+    return model.offsets[0], model.models["bs"].basis_full, model.models["bs"].coff
 
 
-def load_model(device, model_args, topo_loader, save_path_base, envelope_only, epoch_num=-1):
+def load_model(
+    device, model_args, topo_loader, save_path_base, envelope_only, epoch_num=-1
+):
     """
     Important: Make sure prepare the dataset before create the model
     """
     fk = ForwardKinematics(parents=parent_smpl)
-    geo, att, gen = create_envelope_model(device, model_args, topo_loader, is_train=False, parents=parent_smpl)
+    geo, att, gen = create_envelope_model(
+        device, model_args, topo_loader, is_train=False, parents=parent_smpl
+    )
     envelop_model = EnvelopeGenerate(geo, att, gen, fk=fk, args=model_args)
 
     if not envelope_only:
         model_args.fast_train = 0
-        geo2, _, gen2, coff = create_residual_model(device, model_args, topo_loader, is_train=False, parents=parent_smpl,
-                                                    requires_att=False)
-        residual_model = BlendShapesGenerate(geo2, att, gen2, coff, args=model_args, fk=fk)
+        geo2, _, gen2, coff = create_residual_model(
+            device,
+            model_args,
+            topo_loader,
+            is_train=False,
+            parents=parent_smpl,
+            requires_att=False,
+        )
+        residual_model = BlendShapesGenerate(
+            geo2, att, gen2, coff, args=model_args, fk=fk
+        )
 
         sub_models = [geo, att, gen, geo2, gen2, coff]
 
@@ -69,8 +85,9 @@ def load_model(device, model_args, topo_loader, save_path_base, envelope_only, e
 
     for sub_model in sub_models:
         o_path = sub_model.save_path
-        if o_path.endswith('/'): o_path = o_path[:-1]
-        o_path = o_path.split('/')[-1]
+        if o_path.endswith("/"):
+            o_path = o_path[:-1]
+        o_path = o_path.split("/")[-1]
         sub_model.save_path = pjoin(save_path_base, o_path)
         sub_model.load_model(epoch_num)
 
@@ -102,29 +119,31 @@ def prepare_obj(filename, topo_loader):
     return mesh
 
 
-def write_back(prefix, skeleton, skinning_weight, verts, faces, original_path, rot, basis, coff):
+def write_back(
+    prefix, skeleton, skinning_weight, verts, faces, original_path, rot, basis, coff
+):
     os.makedirs(prefix, exist_ok=True)
-    os.makedirs(pjoin(prefix, 'obj'), exist_ok=True)
+    os.makedirs(pjoin(prefix, "obj"), exist_ok=True)
 
     bvh_writer = WriterWrapper(parent_smpl)
     skinning_weight = skinning_weight.detach().cpu().numpy()
-    np.save(pjoin(prefix, 'weight.npy'), skinning_weight)
+    np.save(pjoin(prefix, "weight.npy"), skinning_weight)
     if basis is not None:
         basis = basis.detach().cpu().numpy()
-        np.save(pjoin(prefix, 'basis.npy'), basis.squeeze())
+        np.save(pjoin(prefix, "basis.npy"), basis.squeeze())
     if coff is not None:
         coff = coff.detach().cpu().numpy()
-        np.save(pjoin(prefix, 'coff.npy'), coff.squeeze())
-    bvh_writer.write(pjoin(prefix, 'skeleton.bvh'), skeleton, rot)
+        np.save(pjoin(prefix, "coff.npy"), coff.squeeze())
+    bvh_writer.write(pjoin(prefix, "skeleton.bvh"), skeleton, rot)
 
     os.system(f"cp {original_path} {pjoin(prefix, 'T-pose.obj')}")
 
-    if os.path.exists(pjoin(prefix, 'obj')):
-        os.system(f"rm -r {pjoin(prefix, 'obj/*')}")
+    # if os.path.exists(pjoin(prefix, 'obj')):
+    #     os.system(f"rm -r {pjoin(prefix, 'obj/*')}")
     if verts is not None:
-        print('Writing back...')
+        print("Writing back...")
         for i in tqdm(range(verts.shape[0])):
-            write_obj(pjoin(prefix, 'obj/%05d.obj' % i), verts[i], faces)
+            write_obj(pjoin(prefix, "obj/%05d.obj" % i), verts[i], faces)
 
 
 def main():
@@ -133,18 +152,27 @@ def main():
     device = torch.device(args.device)
 
     train_parser = TrainingOptionParser()
-    model_args = train_parser.load(pjoin(args.model_path, 'args.txt'))
+    model_args = train_parser.load(pjoin(args.model_path, "args.txt"))
     model_args.normalize = args.normalize
 
-    test_pose, test_loc = load_test_anim(args.pose_file, device)
+    # Read from eval_constant/sequences/*.npy
+    if args.pose_file.endswith("npy"):
+        test_pose, _ = load_test_anim(args.pose_file, device)
+    # Read from CMU mocap dataset CMU/*/*_*_poses.npz
+    elif args.pose_file.endswith("npz"):
+        test_pose = load_cmu_mocap_anim(args.pose_file, device)
 
     topo_loader = TopologyLoader(device=device, debug=False)
     mesh = prepare_obj(args.obj_path, topo_loader)
 
-    env_model, res_model = load_model(device, model_args, topo_loader, args.model_path, args.envelope_only)
+    env_model, res_model = load_model(
+        device, model_args, topo_loader, args.model_path, args.envelope_only
+    )
 
     t_pose, topo_id = mesh[0]
-    skinning_weight, skeleton, vs, basis, coff = run_single_mesh(t_pose, topo_id, test_pose, env_model, res_model)
+    skinning_weight, skeleton, vs, basis, coff = run_single_mesh(
+        t_pose, topo_id, test_pose, env_model, res_model
+    )
 
     faces = topo_loader.faces[topo_id]
 
@@ -153,8 +181,18 @@ def main():
     if not args.obj_output:
         vs = None
 
-    write_back(args.result_path, skeleton, skinning_weight, vs, faces, args.obj_path, test_pose, basis, coff)
+    write_back(
+        args.result_path,
+        skeleton,
+        skinning_weight,
+        vs,
+        faces,
+        args.obj_path,
+        test_pose,
+        basis,
+        coff,
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
